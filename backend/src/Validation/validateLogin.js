143a6ddee8e0e550d5bd67462baf5143e512bc18
Sukaran Golani <sk300480@dal.ca>
2022-02-19 14:31:27 -0400

    Intial Commit

--- backend/src/Validation/validateLogin.js
+++
@@ -1,7 +0,0 @@
-export const validateLogin = (data) => {
-  var isValid = false;
-  var validationError = "";
-
-  isValid = true;
-  return { validationError, isValid };
-}
--- backend/src/Validation/validateSignUp.js
+++
@@ -1,6 +0,0 @@
-export const validateSignUp = (data) => {
-  var isValid = false;
-  var validationError = "";
-  isValid = true;
-  return { validationError, isValid };
-};
