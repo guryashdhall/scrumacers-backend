be5f992e14d3ee45368a9bc72376f1c12aaa2298
Guryash Singh Dhall <gr622265@dal.ca>
2022-03-31 17:42:26 -0300

    Refactoring: Removed unnessacary logs

--- src/validation/validateSurvey.js
+++ src/validation/validateSurvey.js
@@ -1,7 +1,6 @@
 const validateSurvey = (data) => {
     let error = new Error();
     error.status = 400;
-    console.log(data)
     if (!data.q1.length || data.q1 == " ") {
         error.message = "Question 1 cannot be empty";
         throw error;
