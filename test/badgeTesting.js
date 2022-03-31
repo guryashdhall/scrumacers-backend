2f8606fc15fa1ff5d181a2af6be62e79e16879e4
Guryash Dhall <guryash.dhall@dal.ca>
2022-03-31 00:35:43 -0300

    Added Test cases for Badge Module

---
+++ test/badgeTesting.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+const app = require("../index");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("Testing of Badge Module", () => {
+  before(done => {
+    app.on('ready', () => done());
+  })
+  beforeEach(() => {
+    data = {
+      emp_id: 7,
+      badge_id: [1, 2]
+    }
+  })
+
+
+  it("Testing for insert/update badge", (done) => {
+    console.log("hiiiii")
+    console.log(data)
+    chai.request(app).put('/api/user/update-employee-badges').set('Authorization', `Bearer ${process.env.MANAGER_TEST_TOKEN}`)
+      .send(data).end((err, res) => {
+        console.log(res)
+        res.should.have.status(200);
+        res.body.should.have.property("message").eqls("Badges updated");
+        done();
+      })
+  })
+  it("Testing for insert/update badge without badge", (done) => {
+    data.badge_id = []
+    chai.request(app).put('/api/user/update-employee-badges').set('Authorization', `Bearer ${process.env.MANAGER_TEST_TOKEN}`)
+      .send(data).end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property("message").eqls("Badges updated");
+        done();
+      })
+  })
+})
