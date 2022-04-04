76c55b7ba8d0fa00262b8dc2b6abdf3aa027795f
@damania <fr454807@dal.ca>
2022-04-04 17:24:14 -0300

    test case for login

--- test/loginTesting.js
+++ test/loginTesting.js
@@ -1,5 +1,5 @@
 const validate_login = require("../src/validation/validateLogin");
-var assert = require('assert');
+var assert = require("assert");
 const app = require("../index");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
@@ -11,40 +11,76 @@ describe("POST api/user/login", () => {
   beforeEach(() => {
     data = {
       email: "jackryan@gmail.com",
-      password: "jackryan3"
-    }
-  })
+      password: "jackryan3",
+    };
+  });
 
   it("Testing for empty email by user", () => {
-    data.email = ""
-    assert.throws(function () { validate_login.validateLogin(data) }, /^Error: Email is empty$/);
-  })
+    data.email = "";
+    assert.throws(function () {
+      validate_login.validateLogin(data);
+    }, /^Error: Email is empty$/);
+  });
   it("Testing for invalid email by user", () => {
-    data.email = "dvgmail.com"
-    assert.throws(function () { validate_login.validateLogin(data) }, /^Error: Email is invalid$/);
-  })
+    data.email = "dvgmail.com";
+    assert.throws(function () {
+      validate_login.validateLogin(data);
+    }, /^Error: Email is invalid$/);
+  });
   it("Testing in case user keeps password empty", () => {
-    data.password = ""
-    assert.throws(function () { validate_login.validateLogin(data) }, /^Error: Password is empty$/);
-  })
+    data.password = "";
+    assert.throws(function () {
+      validate_login.validateLogin(data);
+    }, /^Error: Password is empty$/);
+  });
   it("Testing for required password length", () => {
-    data.password = "abc"
-    assert.throws(function () { validate_login.validateLogin(data) }, /^Error: Password length should be atleast 8$/);
-  })
+    data.password = "abc";
+    assert.throws(function () {
+      validate_login.validateLogin(data);
+    }, /^Error: Password length should be atleast 8$/);
+  });
   it("Testing for wrong user/employee", (done) => {
     data.email = "a@gmail.com";
-    chai.request(app).post('/api/user/login').send(data).end((err, res) => {
-      res.should.have.status(400);
-      res.body.should.have.property("message").eqls("Employee not found");
-      done();
-    })
-  })
+    chai
+      .request(app)
+      .post("/api/user/login")
+      .send(data)
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.have.property("message").eqls("Employee not found");
+        done();
+      });
+  });
   it("Testing for wrong password", (done) => {
     data.password = "jackryan2";
-    chai.request(app).post('/api/user/login').send(data).end((err, res) => {
-      res.should.have.status(400);
-      res.body.should.have.property("message").eqls("Invalid password");
-      done();
-    })
-  })
-})
+    chai
+      .request(app)
+      .post("/api/user/login")
+      .send(data)
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.have.property("message").eqls("Invalid password");
+        done();
+      });
+  });
+
+  it("Testing for login", (done) => {
+    data = {
+      email: "emmabryan@gmail.com",
+
+      password: "emmabryan5",
+    };
+
+    chai
+      .request(app)
+      .post("/api/user/login")
+      .send(data)
+      .end((err, res) => {
+        res.should.have.status(200);
+
+        res.body.should.have.property("message").eqls("login successfully");
+
+        done();
+      });
+  });
+});
